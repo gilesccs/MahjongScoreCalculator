dc499280963e4604d78e265908fc92cd38a1cecc
gilesccs <54528035+gilesccs@users.noreply.github.com>
2020-12-03 18:35:43 +0800

    Added img, css folder, and added basic structure for header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-        <p>
-          Mahjong application
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/gilesccs/MahjongScoreCalculator"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Git Repo
-        </a>
-    </div>
+    <Header></Header>
+    // <div className="App">
+    //     <p>
+    //       Mahjong application
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://github.com/gilesccs/MahjongScoreCalculator"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Git Repo
+    //     </a>
+    // </div>
   );
 }
 
