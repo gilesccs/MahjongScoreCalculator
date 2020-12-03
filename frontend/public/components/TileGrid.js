f0fcfee3d48f1602a57d79340724545238b10677
Alwyn-Ong <52643926+Alwyn-Ong@users.noreply.github.com>
2020-12-03 17:55:00 +0800

    Added TileGrid component

---
+++ frontend/public/components/TileGrid.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class TileGrid extends React.Component{
+    render(){
+        return (
+            <div>
+                <table>
+                    
+                </table>
+            </div>
+        );
+    }
+}
