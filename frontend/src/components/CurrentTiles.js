54de95a12a69e710d3c52cdb4637a1b49a083e6a
gilesccs <54528035+gilesccs@users.noreply.github.com>
2020-12-03 20:13:15 +0800

    Added current tiles grid

--- frontend/src/components/CurrentTiles.js
+++ frontend/src/components/CurrentTiles.js
@@ -1,19 +1,42 @@
 import "../css/CurrentTiles";
 import { useEffect } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+    },
+}));
 
 function CurrentTiles() {
-    useEffect(() => {
-   
-    });
-
+    const classes = useStyles();
+  
     return (
-        <div>
-            <body>
-                test
-            </body>
-        </div>
+    <Grid container className={classes.root} spacing={2}>
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={2}>
+          {[0, 1, 2, 3].map((value) => (
+            <Grid key={value} item>
+              <Paper className={classes.paper} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.control}>
+        </Paper>
+      </Grid>
+    </Grid>
     );
   }
+  
   
   export default CurrentTiles;
   
